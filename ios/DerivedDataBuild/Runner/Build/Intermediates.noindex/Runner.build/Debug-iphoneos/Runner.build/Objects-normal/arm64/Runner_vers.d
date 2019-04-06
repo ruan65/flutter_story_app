c8ca050ddebd6c845e61304b3ef66eddd547ab70
a <ruan65@gmail.com>
2019-04-07 02:30:34 +0300

    addAssets

---
+++ ios/DerivedDataBuild/Runner/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/a/dev/Flutter_apps/stories_app/ios/DerivedDataBuild/Runner/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
